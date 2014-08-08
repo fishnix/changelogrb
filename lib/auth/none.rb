a5f0ae19b99675dfe42c28f80f2b387b0837b788
E Camden Fisher <fish@fishnix.net>
2014-08-08 16:06:13 -0400

    auth none

---
+++ lib/auth/none.rb
@@ -0,0 +1,29 @@
+require 'sinatra/base'
+
+module Sinatra
+  module NoneAuth
+
+    module Helpers
+      def authorize!
+        session[:authorized] = true
+        session[:user_id] = 'user'
+        true
+      end
+
+      def logout!
+        session[:user_id] = nil
+        session[:authorized] = false
+      end
+    end
+
+    def self.registered(app)
+      app.helpers NoneAuth::Helpers
+      
+      app.get '/logout' do
+        logout!
+      end
+    end
+  end
+
+  register NoneAuth
+end
